43e3de2ed77987070ca9ca9774bfacb9209c417e
rauatorres <rauatorres@gmail.com>
2026-08-24 19:04:56 -0300

    adicionando url às configs de conexão ao banco

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -6,6 +6,7 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'postgres',
+        url: process.env.DB_URL,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT ? +process.env.DB_PORT : 5432,
         username: process.env.DB_USER,
